ecae5c528342d67325ebac84eee95a17b1e3d511
zhc-cat <z1109548274@163.com>
2020-06-12 08:26:41 +0800

    developer 001

---
+++ src/main/java/com/zhc/gittest/GitMain.java
@@ -0,0 +1,11 @@
+package com.zhc.gittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitMain {
+    public static void main(String []args){
+        SpringApplication.run(GitMain.class, args);
+    }
+}
---
+++ src/main/java/com/zhc/gittest/controller/GItController.java
@@ -0,0 +1,8 @@
+package com.zhc.gittest.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GItController {
+
+}
